05741e3be03db915c0a273b6c4bb26ee63c061e6
hugoM-2 <lalougo.moutard@gmail.com>
2020-02-05 11:59:45 +0100

    Canva 1 M

---
+++ Assets/Hub/Script Hub/UIM.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class UIM : MonoBehaviour
+{
+    
+
+    
+    public void Retour(int SceneIndex)
+    {
+        SceneManager.LoadScene(1);
+
+
+
+
+    }
+
+    public void ChangeScene(int SceneIndex)
+    {
+        SceneManager.LoadScene(2);
+    }
+
+
+}
