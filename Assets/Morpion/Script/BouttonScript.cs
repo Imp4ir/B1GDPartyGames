c7a9189b70ce92df7740b95f449f5f110d454a63
hugoM-2 <lalougo.moutard@gmail.com>
2020-02-19 11:53:10 +0100

    Début Morpion

---
+++ Assets/Morpion/Script/BouttonScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+
+    GameManager gmScript;
+
+    private void Start()
+    {
+        gmScript = GameObject.Find("Canvas").GetComponent<GameManager>();
+    }
+    public void Select()
+    {
+
+        transform.Find("Text").GetComponent<Text>().text = "X";
+        GetComponent<Button>().interactable = false;
+        int index = int.Parse(gameObject.name);
+
+        gmScript.matrix[index] = "X";
+
+        if (gmScript.verification("X"))
+        {
+            gmScript.ShowPanel("X");
+        }
+        else
+        {
+            gmScript.ComputerPlay();
+        }
+    }
+}
